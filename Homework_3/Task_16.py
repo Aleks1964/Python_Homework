cd96f14653c170026c8d0d48c00c2eb21b6cb82d
Aleksandr <sveta-sasha-21@yandex.ru>
2023-02-28 22:50:17 +0300

    Поправил файл Задачи 16 - выход по неверному числу элементов.

--- Homework_3/Task_16.py
+++ Homework_3/Task_16.py
@@ -10,18 +10,20 @@
 # 3
 # -> 1
 
+import sys
 N = int(input(f'Введите число элементов массива '))
 massiv = [int(i) for i in input('Введите через пробел целые числа - элементы массива: ').split()]
 count = 0
 if N != len(massiv):
     print('Введённый Вами массив не соответствует введённому ранее количеству элементов массива.')
+    sys.exit()
 else:   
     X = int(input(f'Введите искомое в массиве число '))
     for i in massiv:
 	    if i == X:
 		    count += 1
-    if count == 0:
-        print(f'Число {X} НЕ встречается в массиве {massiv}.')
-    else:
-        print(f'-> {count}')
-        print(f'Число {X} встречается в массиве {massiv} {count} раза.')
+if count == 0:
+    print(f'Число {X} НЕ встречается в массиве {massiv}.')
+else:
+    print(f'-> {count}')
+    print(f'Число {X} встречается в массиве {massiv} {count} раза.')
